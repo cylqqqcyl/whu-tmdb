78ec5479a2850b711af07b5623ccd0fdfb99c60a
dzfcs <729159553@qq.com>
2023-05-06 16:49:31 +0800

    six people

--- app/src/main/java/drz/tmdb/map/TrajectoryUtils.java
+++ app/src/main/java/drz/tmdb/map/TrajectoryUtils.java
@@ -35,13 +35,25 @@ public class TrajectoryUtils {
     // 初始化两张轨迹表
     public void init(){
         try{
-            String sql1 = "CREATE CLASS mobile_phone_traj (trajectory_id int,user_id char, trajectory char);";
-            String sql2 = "CREATE CLASS watch_traj (trajectory_id int,user_id char, trajectory char);";
+            String sql1 = "CREATE CLASS cyl_traj (trajectory_id int,user_id char, trajectory char);";
+            String sql2 = "CREATE CLASS dzf_traj (trajectory_id int,user_id char, trajectory char);";
+            String sql3 = "CREATE CLASS kdy_traj (trajectory_id int,user_id char, trajectory char);";
+            String sql4 = "CREATE CLASS wxz_traj (trajectory_id int,user_id char, trajectory char);";
+            String sql5 = "CREATE CLASS hr_traj (trajectory_id int,user_id char, trajectory char);";
+            String sql6 = "CREATE CLASS wql_traj (trajectory_id int,user_id char, trajectory char);";
             Create create = new CreateImpl(memConnect);
             Statement parse1 = CCJSqlParserUtil.parse(sql1);
             Statement parse2 = CCJSqlParserUtil.parse(sql2);
+            Statement parse3 = CCJSqlParserUtil.parse(sql3);
+            Statement parse4 = CCJSqlParserUtil.parse(sql4);
+            Statement parse5 = CCJSqlParserUtil.parse(sql5);
+            Statement parse6 = CCJSqlParserUtil.parse(sql6);
             create.create(parse1);
             create.create(parse2);
+            create.create(parse3);
+            create.create(parse4);
+            create.create(parse5);
+            create.create(parse6);
         }catch (Exception e){
             e.printStackTrace();
         }
@@ -57,12 +69,33 @@ public class TrajectoryUtils {
         String tString = "'" + serialize(trajectory) + "'"; // 序列化后的位置信息
 
         // 构造SQL语句
-        String sql;
-        if(tID % 2 == 1){
-            sql = String.format("INSERT INTO mobile_phone_traj VALUES (%d,%s,%s);", tID, uID, tString);
-        }else{
-            sql = String.format("INSERT INTO watch_traj VALUES (%d,%s,%s);", tID, uID, tString);
+        String sql="";
+        switch (tID%6){
+            case 0:
+                sql = String.format("INSERT INTO cyl_traj VALUES (%d,%s,%s);", tID, uID, tString);
+                break;
+            case 1:
+                sql = String.format("INSERT INTO dzf_traj VALUES (%d,%s,%s);", tID, uID, tString);
+                break;
+            case 2:
+                sql = String.format("INSERT INTO kdy_traj VALUES (%d,%s,%s);", tID, uID, tString);
+                break;
+            case 3:
+                sql = String.format("INSERT INTO wxz_traj VALUES (%d,%s,%s);", tID, uID, tString);
+                break;
+            case 4:
+                sql = String.format("INSERT INTO hr_traj VALUES (%d,%s,%s);", tID, uID, tString);
+                break;
+            case 5:
+                sql = String.format("INSERT INTO wql_traj VALUES (%d,%s,%s);", tID, uID, tString);
+                break;
+            default:break;
         }
+//        if(tID % 6 == ){
+//            sql = String.format("INSERT INTO mobile_phone_traj VALUES (%d,%s,%s);", tID, uID, tString);
+//        }else{
+//            sql = String.format("INSERT INTO watch_traj VALUES (%d,%s,%s);", tID, uID, tString);
+//        }
 
         // 执行SQl语句
         try {
@@ -78,18 +111,42 @@ public class TrajectoryUtils {
     // 读取历史轨迹数据，每条轨迹是若干个轨迹点组成的ArrayList
     public static ArrayList<ArrayList<TrajectoryPoint>> load(){
         ArrayList<ArrayList<TrajectoryPoint>> ret = new ArrayList<ArrayList<TrajectoryPoint>>();
-        String sql1 = "SELECT * FROM mobile_phone_traj;";
-        String sql2 = "SELECT * FROM watch_traj;";
+        String sql1 = "SELECT * FROM cyl_traj;";
+        String sql2 = "SELECT * FROM dzf_traj;";
+        String sql3 = "SELECT * FROM kdy_traj;";
+        String sql4 = "SELECT * FROM wxz_traj;";
+        String sql5 = "SELECT * FROM hr_traj;";
+        String sql6 = "SELECT * FROM wql_traj;";
         try{
             Select select = new SelectImpl(memConnect);
             Statement parse1 = CCJSqlParserUtil.parse(sql1);
             SelectResult result1 = select.select(parse1);
             Statement parse2 = CCJSqlParserUtil.parse(sql2);
             SelectResult result2 = select.select(parse2);
+            Statement parse3 = CCJSqlParserUtil.parse(sql3);
+            SelectResult result3 = select.select(parse3);
+            Statement parse4 = CCJSqlParserUtil.parse(sql4);
+            SelectResult result4 = select.select(parse4);
+            Statement parse5 = CCJSqlParserUtil.parse(sql5);
+            SelectResult result5 = select.select(parse5);
+            Statement parse6 = CCJSqlParserUtil.parse(sql6);
+            SelectResult result6 = select.select(parse6);
             for(Tuple t: result1.getTpl().tuplelist){
                 ret.add(deserialize((String) t.tuple[2]));
             }
             for(Tuple t: result2.getTpl().tuplelist){
+                ret.add(deserialize((String) t.tuple[2]));
+            }
+            for(Tuple t: result3.getTpl().tuplelist){
+                ret.add(deserialize((String) t.tuple[2]));
+            }
+            for(Tuple t: result4.getTpl().tuplelist){
+                ret.add(deserialize((String) t.tuple[2]));
+            }
+            for(Tuple t: result5.getTpl().tuplelist){
+                ret.add(deserialize((String) t.tuple[2]));
+            }
+            for(Tuple t: result6.getTpl().tuplelist){
                 ret.add(deserialize((String) t.tuple[2]));
             }
         }catch (Exception e){
